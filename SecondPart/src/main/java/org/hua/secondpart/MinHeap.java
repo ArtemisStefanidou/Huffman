0d7c524ac8e7560f53fbfa0746ed0bca1d05243d
Vasilis <anxheldohyskaj@gmail.com>
2020-12-07 11:44:30 +0200

    changes

---
+++ SecondPart/src/main/java/org/hua/secondpart/MinHeap.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hua.secondpart;
+
+/**
+ * A min-heap
+ *
+ * @param <E> the element type
+ */
+public interface MinHeap<E extends Comparable<E>> {
+
+	/**
+	 * Insert an element into the heap
+	 * 
+	 * @param elem the element
+	 */
+	void insert(E elem);
+
+	/**
+	 * Find the minimum element
+	 * 
+	 * @return the minimum element
+	 */
+	E findMin();
+
+	/**
+	 * Delete the minimum element
+	 * 
+	 * @return the deleted element
+	 */
+	E deleteMin();
+
+	/**
+	 * Check if the heap is empty
+	 * 
+	 * @return true if empty, false otherwise
+	 */
+	boolean isEmpty();
+
+	/**
+	 * Get the size of the heap
+	 * 
+	 * @return the size of the heap
+	 */
+	int size();
+
+	/**
+	 * Clear the heap
+	 */
+	void clear();
+
+}
