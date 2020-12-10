d370aaf2e72a615ff5d94fb73346d709f3ee83e9
ArtemisStef <rouliscat2000@gmail.com>
2020-12-10 20:15:14 +0200

    trycatch

--- Part1/src/main/java/org/hua/firstpart/App.java
+++ Part1/src/main/java/org/hua/firstpart/App.java
@@ -8,67 +8,55 @@ package org.hua.firstpart;
 /**
  *
  * @author Αντωνόπουλος Διογένης
- * @author Στεφανίδου   Άρτεμις
- * @author  Χύσκαϊ      Βασίλης
+ * @author Στεφανίδου Άρτεμις
+ * @author Χύσκαϊ Βασίλης
  */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class App {
+public class App {    
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException {
 
         //Array for the url
         URL[] url = new URL[3];
 
-        try {
-            
-            //URL given fron user
-            url[0] = new URL ("https://www.gutenberg.org/files/1342/1342-0.txt");
-            url[1] = new URL ("https://www.gutenberg.org/files/11/11-0.txt");
-            url[2] = new URL ("https://www.gutenberg.org/files/2701/2701-0.txt");
-        
-            BufferedReader[] reader = new BufferedReader[3];
+        //URL given fron user
+        url[0] = new URL("https://www.gutenberg.org/files/1342/1342-0.txt");
+        url[1] = new URL("https://www.gutenberg.org/files/11/11-0.txt");
+        url[2] = new URL("https://www.gutenberg.org/files/2701/2701-0.txt");
 
-            //each position of the array corresponds to the counter of an ASCII character
-            int[] chars = new int[128];
-            int charValue = 0;
-            
-            //For the three url
-            for (int i = 0; i < 3; i++) {
-                
-                reader[i] = new BufferedReader(new InputStreamReader(url[i].openStream()));
+        BufferedReader[] reader = new BufferedReader[3];
 
-                while ((charValue = reader[i].read()) != -1) {
-                    if (charValue < 128) {
-                        chars[charValue]++;
-                    }
+        //each position of the array corresponds to the counter of an ASCII character
+        int[] chars = new int[128];
+        int charValue = 0;
+
+        //For the three url
+        for (int i = 0; i < 3; i++) {
+
+            reader[i] = new BufferedReader(new InputStreamReader(url[i].openStream()));
+
+            while ((charValue = reader[i].read()) != -1) {
+                if (charValue < 128) {
+                    chars[charValue]++;
                 }
-
-                reader[i].close();
             }
-            //Create a file and print the results there
-            BufferedWriter outputStream = new BufferedWriter(new FileWriter("frequencies.dat"));
 
+            reader[i].close();
+        }
+        //Create a file and print the results there
+        try (BufferedWriter outputStream = new BufferedWriter(new FileWriter("frequencies.dat"))) {
             for (int i = 0; i < 128; i++) {
                 outputStream.write(i + " -> " + chars[i] + "\n");
                 outputStream.flush();
             }
-
-            outputStream.close();
         }
-        // Check for exceptions
-        catch (IndexOutOfBoundsException e ) {
-            System.out.println("Your index is out of bounds" );
-        }catch (NullPointerException e){
-            System.out.println("Some of the arrays are null");
-        }catch (Exception e){
-            System.out.println("Something went wrong");
-        }
-        
     }
 
 }
