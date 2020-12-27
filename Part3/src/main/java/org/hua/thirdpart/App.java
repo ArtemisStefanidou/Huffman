8c9eb315c5135564970ba2456ae8de8d87a8aa04
ArtemisStef <rouliscat2000@gmail.com>
2020-12-27 17:14:17 +0200

    updates

--- Part3/src/main/java/org/hua/thirdpart/App.java
+++ Part3/src/main/java/org/hua/thirdpart/App.java
@@ -17,11 +17,10 @@ import java.net.URL;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class App {    
+public class App {
 
     public static void main(String[] args) throws MalformedURLException, IOException, ClassNotFoundException {
 
-  
         URL[] url = new URL[3];
 
         //URL given from user
@@ -49,9 +48,9 @@ public class App {
             reader[i].close();
         }
         //Create a file and print the results there
-        try (BufferedWriter outputStream = new BufferedWriter(new FileWriter("frequencies.dat"))) {
+        try ( BufferedWriter outputStream = new BufferedWriter(new FileWriter("frequencies.dat"))) {
             for (int i = 0; i < 128; i++) {
-                outputStream.write( chars[i] + "\n");
+                outputStream.write(chars[i] + "\n");
                 outputStream.flush();
             }
         }
@@ -59,7 +58,7 @@ public class App {
         //Read the frequencies
         Scanner scanner = new Scanner(new File("frequencies.dat"));
         int[] array = new int[128];
-        for(int i= 0; i < 128; i++) {
+        for (int i = 0; i < 128; i++) {
             array[i] = scanner.nextInt();
         }
 
@@ -67,46 +66,31 @@ public class App {
         Huffman tree = new Huffman();
 
         //Create the huffman tree by calling makeTree and write it in tree.dat
-        try (ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream("tree.dat"))) {
+        try ( ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream("tree.dat"))) {
             objectOut.writeObject(tree.makeTree(array));
         }
-        
-        ObjectInput input = new ObjectInputStream(new BufferedInputStream(new FileInputStream("tree.dat")));
-        
-        //ReadTree root = new ReadTree((Node) input.readObject());
-        
-       
-        
-        try{
-            
-            
-        
-        Node root =(Node) input.readObject();
-        ReadTree Tree =new ReadTree();
-       
-        
-        
-        PrintWriter output= new PrintWriter("codes.dat");
-        
-        Tree.printCode(root,output);
-        
-       
-        output.close();
-        
-        
-        
-        }catch(NoSuchElementException e){
-            System.out.print("");
-        }
-        
-       
-        
-        
-        //Tree.printHuffmanTree(root,"");
-        
-        
 
-        
+        ObjectInput input = new ObjectInputStream(new BufferedInputStream(new FileInputStream("tree.dat")));
+
+        try {
+
+            Node root = (Node) input.readObject();
+            Huffman read = new Huffman();
+
+            try (BufferedWriter output = new BufferedWriter(new FileWriter("codes.dat"))) {
+                String[] arrayCode = read.printCode(root);
+                
+                for (int i = 0; i < 128; i++) {
+                    
+                    output.write(i + "->" + arrayCode[i] + "\n");
+                    output.flush();
+                }
+            }
+
+        } catch (NoSuchElementException e) {
+            System.out.print("error");
+        }
+
     }
 
 }
