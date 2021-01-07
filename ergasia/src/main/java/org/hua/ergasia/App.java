ac3f624f8684788f1dab8601d1526814b26103c2
ArtemisStef <rouliscat2000@gmail.com>
2021-01-07 17:16:54 +0200

    new

--- ergasia/src/main/java/org/hua/ergasia/App.java
+++ ergasia/src/main/java/org/hua/ergasia/App.java
@@ -78,18 +78,71 @@ public class App {
         try ( BufferedWriter output = new BufferedWriter(new FileWriter("codes.dat"))) {
             
             //save the array that return from class Huffman method printCode
-            String[] arrayCode = tree.printCode(root);
+            String[] arrayCode = tree.createCode(root);
 
             //print the result of representation of characters in the file codes.dat
             for (int i = 0; i < 128; i++) {
 
-                output.write(i + "->" + arrayCode[i] + "\n");
+                output.write(arrayCode[i] + "\n");
                 output.flush();
             }
             output.close();
             System.out.println("A file created.Check codes.dat");
         }
+         //Check for correct input from the terminal
+        if (args.length < 2) {
+            System.out.println("Usage: program filename filename");
+            System.exit(-1);
+	    }
         
+        
+        //Read the Huffman coding from the file codes.dat
+        String[] code = new String[128];
+        try (Scanner myScanner = new Scanner(new File("codes.dat"))) {
+            int i = 0;
+            while (myScanner.hasNextLine()) {
+                code[i++] = myScanner.nextLine();
+            }
+        }
+        
+        //Store in encode,char by char,the coresponding huffman encoding
+        File file = new File(args[0]);
+        StringBuffer encode = new StringBuffer();
+        try (BufferedReader myReader = new BufferedReader(new FileReader(file))) {
+            int ch;
+            while ((ch = myReader.read()) != -1) {
+                encode.append(code[ch]);
+            }
+        }
+
+        //Find the useful bits of the last byte
+        Integer tmp;
+        tmp = encode.length() % 8;
+        
+        //Create and output the new bytes in the file
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(args[1]))) {
+            //Print the usesful bits and a newline
+            dataOut.writeBytes(tmp.toString());
+            dataOut.writeChar(10);
+            
+            byte currentByte = 0;
+            int i = 0, j = 0;
+            while (i < encode.length()) {
+                //Do the required bitwise operations 
+                if (encode.charAt(i) == '1') {
+                    currentByte |= 1 << (7 - j);
+                }
+                i++;
+                j++;
+                //Print to the file if you have 8 bits or at your last bits
+                if (i % 8 == 0 || i == encode.length() - 1) {
+                    
+                    dataOut.writeByte(currentByte);
+                    j = 0;
+                    currentByte = 0;
+                }
+            }
+        }
 
     }
 
